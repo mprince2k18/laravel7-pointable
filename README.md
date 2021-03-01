faea544d9b6ba0fc34d9ac25ad5532fedf4625f6
mprince2k20 <mprince2k16@gmail.com>
2021-03-01 10:25:03 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,98 @@
+[![Latest Stable Version](https://poser.pugx.org/mprince/laravel-pointable/v/stable)](https://packagist.org/packages/mprince/laravel-pointable)
+[![Total Downloads](https://poser.pugx.org/mprince/laravel-pointable/downloads)](https://packagist.org/packages/mprince/laravel-pointable)
+[![Latest Unstable Version](https://poser.pugx.org/mprince/laravel-pointable/v/unstable)](https://packagist.org/packages/mprince/laravel-pointable) 
+[![License](https://poser.pugx.org/mprince/laravel-pointable/license)](https://packagist.org/packages/mprince/laravel-pointable)
+
+# Laravel Pointable
+
+Point Transaction system for Laravel 8.*
+
+Inspired from [Trexology](https://github.com/Trexology/laravel-pointable)
+
+## Installation
+
+First, pull in the package through Composer.
+
+```js
+composer require mprince/laravel-pointable
+```
+
+And then include the service provider within `app/config/app.php`.
+
+```php
+'providers' => [
+    Mprince\Pointable\PointableServiceProvider::class
+];
+```
+
+At last you need to publish.
+```
+php artisan vendor:publish --provider="Mprince\Pointable\PointableServiceProvider"
+```
+
+and then run the migration.
+
+```
+php artisan migrate
+```
+
+-----
+
+### Setup a Model
+```php
+<?php
+
+namespace App;
+
+use Mprince\Pointable\Contracts\Pointable;
+use Mprince\Pointable\Traits\Pointable as PointableTrait;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model implements Pointable
+{
+    use PointableTrait;
+}
+```
+
+### Add Points
+```php
+$user = User::first();
+$amount = 10; // (Double) Can be a negative value
+$message = "The reason for this transaction";
+
+//Optional (if you modify the point_transaction table)
+$data = [
+    'ref_id' => 'someReferId',
+];
+
+$transaction = $user->addPoints($amount,$message,$data);
+
+dd($transaction);
+```
+
+### Get Current Points
+```php
+$user = User::first();
+$points = $user->currentPoints();
+
+dd($points);
+```
+
+### Get Transactions
+```php
+$user = User::first();
+$user->transactions;
+
+//OR
+$user['transactions'] = $user->transactions(2)->get(); //Get last 2 transactions
+
+dd($user);
+```
+
+### Count Transactions
+```php
+$user = User::first();
+$user['transactions_total'] = $user->countTransactions();
+
+dd($user);
+```
---
+++ vendor/composer/installed.php
@@ -0,0 +1,137 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'mprince/laravel-pointable',
+  ),
+  'versions' => 
+  array (
+    'doctrine/inflector' => 
+    array (
+      'pretty_version' => '2.1.x-dev',
+      'version' => '2.1.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6a0da4f0e06aa5cd83a2c1a4e449fae98c8bad7',
+    ),
+    'illuminate/contracts' => 
+    array (
+      'pretty_version' => '7.x-dev',
+      'version' => '7.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7d964384f0283bd7525ae7b5baa7ad32e5503b8e',
+    ),
+    'illuminate/support' => 
+    array (
+      'pretty_version' => 'v7.30.4',
+      'version' => '7.30.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1c95b8f842308ff15a56d29d897d3bda29001f1c',
+    ),
+    'mprince/laravel-pointable' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'nesbot/carbon' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '2.x-dev',
+      ),
+      'reference' => '2fd2c4a77d58a4e95234c8a61c5df1f157a91bf4',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.1.x-dev',
+      ),
+      'reference' => '381524e8568e07f31d504a945b88556548c8c42e',
+    ),
+    'psr/simple-cache' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.0.x-dev',
+      ),
+      'reference' => '5a7b96b1dda5d957e01bc1bfe77dcca09c5a7474',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.22.x-dev',
+      ),
+      'reference' => '5232de97ee3b75b0360528dae24e73db49566ab1',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.22.x-dev',
+      ),
+      'reference' => 'dc3063ba22c2a1fd2f45ed856374d79114998f91',
+    ),
+    'symfony/translation' => 
+    array (
+      'pretty_version' => '5.x-dev',
+      'version' => '5.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '84f3e8172eee0a97ba3506e30233472fb415d8c4',
+    ),
+    'symfony/translation-contracts' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '2.4.x-dev',
+      ),
+      'reference' => '502490047e9064e6f1bc331ab1d43ca3da68983d',
+    ),
+    'symfony/translation-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '2.3',
+      ),
+    ),
+    'voku/portable-ascii' => 
+    array (
+      'pretty_version' => '1.5.6',
+      'version' => '1.5.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '80953678b19901e5165c56752d087fc11526017c',
+    ),
+  ),
+);
